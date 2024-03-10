44324bc5a97a59b25945fbc54ec6449c05187e0c
Matthew Britt-Webb <matthewbritt-webb2025@u.northwestern.edu>
2024-03-09 20:52:41 -0700

    new scheduling

---
+++ Package-Engine/pages/6_📖_Future Bookings
@@ -0,0 +1,27 @@
+import streamlit as st
+import pandas as pd
+from datetime import date, timedelta
+from pages.future_bookings.bookings import process_daycare, process_boarding, combine_data
+
+st.title("Future Bookings")
+
+st.subheader("Upload Uninvoiced booking daycare")
+
+daycare_file = st.file_uploader(" Upload 'BoardingOccupancy.xls'")
+if not daycare_file == None: 
+    st.success(f"successfully uploaded {daycare_file.name}!")
+    daycare_df = process_daycare(daycare_file)
+
+boarding_file = st.subheader("Upload Uninvoiced booking boarding")
+if not boarding_file == None: 
+    st.success(f"successfully uploaded {boarding_file.name}!")
+    boarding_df = process_boarding(boarding_file)
+
+
+if st.button('Run Analysis'):
+    
+    table = combine_data(daycare_df, boarding_df)
+    st.text('Press Download First Visit')
+    csv = table.to_csv().encode('utf-8')
+    st.download_button(label='Download Data', data=csv, 
+        file_name=f'DailyHeadcount_{str(date.today())}.csv', mime='text/csv')
---
+++ Package-Engine/pages/future_bookings/bookings.py
@@ -0,0 +1,60 @@
+import pandas as pd
+import numpy as np
+
+def process_daycare(file):
+    daycare = pd.read_excel(file, header=5)
+    daycare = daycare.filter(regex='^(?!Unnamed)')
+    daycare.dropna(inplace=True)
+    daycare['Date'] = daycare['In Date']
+    daycare['Dog'] = daycare['Customer'].apply(lambda x: x.split(':')[1])
+    daycare['Customer'] = daycare['Customer'].apply(lambda x: x.split(':')[0])
+    daycare = daycare[['Date', 'Customer', 'Dog', 'In Date', 'Out Date']]
+    daycare['Daycare'] = 1
+    daycare['board_arrive'] = 0
+    daycare['board_continue'] = 0
+    daycare['board_depart'] = 0
+
+    return daycare
+
+def process_boarding(file):
+
+    boarding = pd.read_excel(file, header=5)
+    boarding = boarding.filter(regex='^(?!Unnamed)')
+    boarding.dropna(inplace=True)
+    boarding['Date'] = boarding['In Date']
+    boarding['Dog'] = boarding['Customer'].apply(lambda x: x.split(':')[1])
+    boarding['Customer'] = boarding['Customer'].apply(lambda x: x.split(':')[0])
+    boarding = boarding[['Date', 'Customer', 'Dog', 'In Date', 'Out Date']]
+
+    boarding['In Date'] = pd.to_datetime(boarding['In Date'])
+    boarding['Out Date'] = pd.to_datetime(boarding['Out Date'])
+
+    # Create a new DataFrame to store the expanded rows
+    expanded_rows = []
+
+    # Iterate over each row in the original DataFrame
+    for _, row in boarding.iterrows():
+        # Generate a date range between 'In Date' and 'Out Date' inclusive
+        date_range = pd.date_range(row['In Date'], row['Out Date'], freq='D')
+        
+        # Create a new row for each date in the range
+        for date in date_range:
+            new_row = row.copy()
+            new_row['Date'] = date
+            expanded_rows.append(new_row)
+            
+    # Create a new DataFrame from the list of expanded rows
+    expanded_boarding = pd.DataFrame(expanded_rows)
+
+    # Reset index to get a clean index for the new DataFrame
+    expanded_boarding.reset_index(drop=True, inplace=True)
+
+    return expanded_boarding
+
+def combine_data(daycare, boarding):
+    df = pd.concat([daycare, boarding])
+    df.sort_values('Date', inplace=True)
+    df.fillna(0, inplace=True)
+    df['Daycare'] = df['Daycare'].astype(int)
+
+    return df
